f7bd047698a51f813edf6ee26690e76f78c292b0
Aleksei Gornev <vigadeparandus@gmail.com>
2022-03-02 16:15:29 +0200

    ALEKSEI-TASK1 added controllerGetNewTicket

--- src/main/java/ee/agileworks/proovitoo/helpdesk/TicketController.java
+++ src/main/java/ee/agileworks/proovitoo/helpdesk/TicketController.java
@@ -5,14 +5,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("/helpdesk")
 public class TicketController {
 
     public static Ticket ticket = new Ticket();
 
+    @Resource
+    private TicketService ticketService;
+
     @GetMapping("/ticket")
     public Ticket getTicket() {
         return ticket;
+    }
+
+    @GetMapping("/new/ticket")
+    public TicketDto controllerGetNewTicket() {
+        TicketDto newTicket = ticketService.createNewTicket();
+        return newTicket;
     }
 }
