16b26a1b1dad1db272ff80711580b6c61ebc28ab
Aleksei Gornev <vigadeparandus@gmail.com>
2022-03-07 13:43:50 +0200

    ALEKSEI-TASK1 changed dueDateMinusOneHour

--- src/main/java/ee/agileworks/proovitoo/helpdesk/RequestResult.java
+++ src/main/java/ee/agileworks/proovitoo/helpdesk/RequestResult.java
@@ -3,14 +3,18 @@ package ee.agileworks.proovitoo.helpdesk;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Service
 public class RequestResult {
     private String userName;
     private String description;
     private LocalDateTime enteredDate;
     private LocalDateTime dueDate;
+    private LocalDateTime dueDateMinusOneHour;
+//    private LocalDateTime timeNow;
 }
--- src/main/java/ee/agileworks/proovitoo/helpdesk/TicketService.java
+++ src/main/java/ee/agileworks/proovitoo/helpdesk/TicketService.java
@@ -1,15 +1,23 @@
 package ee.agileworks.proovitoo.helpdesk;
 
+import org.hibernate.type.LocalDateTimeType;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 
 @Service
 public class TicketService {
 
     @Resource
     private TicketSystem ticketSystem;
+
+    @Resource
+    private RequestResult requestResult;
 
     public RequestResult addNewTicket(TicketDto ticketDto) {
         RequestResult ticket = new RequestResult();
@@ -18,6 +26,16 @@ public class TicketService {
         ticket.setEnteredDate(LocalDateTime.now());
         ticket.setDueDate(ticketDto.getDueDate());
         ticketSystem.incrementTicketIdCounter();
+        ticket.setDueDateMinusOneHour(ticketDto.getDueDate().minusHours(3)); // -1h + (-2h) to take into account new Date() time in CET in front end
+//        ticket.setTimeNow(LocalDateTime.now());
         return ticket;
     }
+
+//    public LocalDateTime calculateDueDateMinusHour(TicketDto ticketDto) {
+//        Instant minusOneHour = Instant.from(ticketDto.getDueDate()).minus(1, ChronoUnit.HOURS);
+//        LocalDateTime dueDateMinusOneHour = ticketDto.getDueDate().minusHours(1);
+//        return dueDateMinusOneHour;
+//    }
+
+
 }
